01b4ff3ec6698bd57756721397094fd3b409c18c
komalrani12 <kopuma2003@gmail.com>
2024-08-12 16:55:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function applyNow() {
+    window.location.href = "https://www.cognifyz.com/apply"; 
+}
+
+function learnMore() {
+    window.location.href = "https://www.cognifyz.com/internship"; 
+}
